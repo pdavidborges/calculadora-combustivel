d9176da8ae8561929baf7cb1d4e9e1790459aa7f
David Borges <112510561+pdavidborges@users.noreply.github.com>
2026-06-18 12:14:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//alert('JS Carregado');
+
+//Função que só será executada quando o usuário enviar o formulário
+
+function calcularCombustivel(e){
+    //console.log('Função acionada');
+    e.preventDefault(); //Previnindo o comportamento padrão de quem acionou a função
+    //console.log(e);
+
+    //Seleção de DOM
+    var gasolina = document.getElementById('gasolina').value;
+    var etanol = document.getElementById('etanol').value;
+    var resposta = document.getElementById('resposta');
+
+    var resultado = etanol / gasolina;
+
+    if(resultado < 0.7){
+        //alert('Abasteça com Etanol');
+        resposta.innerHTML = 'Abasteça com Etanol';
+    }
+
+    else{
+        //alert('Abasteça com Gasolina');
+        resposta.innerHTML = 'Abasteça com Gasolina';
+    }
+        
+}
+
